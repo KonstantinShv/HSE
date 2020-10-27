6c471051b9f06ef1b746da3a35fc266df8b4d1c2
KonstantinShv <shundeev1999@mail.ru>
2020-10-27 14:01:05 +0400

    3

---
+++ Lab2/main.cpp
@@ -0,0 +1,51 @@
+#include "rccregisters.hpp"
+#include "gpiocregisters.hpp"
+#include <iostream>
+
+
+int main()
+{  
+    
+  
+  
+  RCC::CR::HSEON::On::Set();
+  
+  while(!RCC::CR::HSERDY::Ready::IsSet())
+  {
+  }
+  
+    
+  
+  RCC::CFGR::SW::Hse::Set();
+  while(!RCC::CFGR::SWS::Hse::IsSet())
+  {
+  }
+  
+ 
+  RCC::CR::HSION::Off::Set();
+
+  
+  
+  RCC::AHB1ENR::GPIOCEN::Enable::Set();
+  //Подать тактирование на порт С
+   //*reinterpret_cast<std::uint32_t *>(0x40023830) = 1U << 2U ;    
+  //Настроить PortC.5 на выход. Регистр PortC.MODER5, Адрес см. https://www.st.com/resource/en/datasheet/stm32f411re.pdf  стр 54
+  // Описание регистра см https://www.st.com/resource/en/reference_manual/dm00119316-stm32f411xce-advanced-armbased-32bit-mcus-stmicroelectronics.pdf стр. 157
+  //*reinterpret_cast<std::uint32_t *>(0x40020800) = 1U << 10U;
+  GPIOC::MODER::MODER5::Output::Set();
+  for(;;)
+  {
+    for (int i = 0; i < 1000000; i ++)
+    {
+    }
+    GPIOC::ODR::ODR5::High::Set();
+  //Вывести 1 в PortC.5. Регистр ODR м https://www.st.com/resource/en/reference_manual/dm00119316-stm32f411xce-advanced-armbased-32bit-mcus-stmicroelectronics.pdf стр. 159
+  //*reinterpret_cast<std::uint32_t *>(0x40020814) = 1U << 5U;
+  //GPIOC::ODR::ODR5::Low::Set();
+    for (int i = 0; i < 1000000; i ++)
+    {
+    }
+    GPIOC::ODR::ODR5::Low::Set();
+  }
+  return 1;
+}
